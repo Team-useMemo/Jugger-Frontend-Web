4825e1374a532ab7e5fdf3d6ea46341c187ade3f
Jadest <Jadest13x03@gmail.com>
2025-03-11 00:44:21 +0900

    #1 [style] 폰트 시스템(일부), 컬러 시스템 적용

--- src/pages/Login/page.tsx
+++ src/pages/Login/page.tsx
@@ -1,5 +1,17 @@
+import styled from '@emotion/styled';
+import { theme } from '../../styles/theme';
+
+const Tmp = styled.div({
+  color: theme.colors.cornflowerblue,
+});
+
 const LoginPage = () => {
-  return <div>loginPage</div>;
+  return (
+    <div>
+      loginPage
+      <Tmp>asd</Tmp>
+    </div>
+  );
 };
 
 export default LoginPage;
