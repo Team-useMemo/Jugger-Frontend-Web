4de2e30f30d722b5c7ec7c267820eb58f5f7db85
Jadest <Jadest13x03@gmail.com>
2025-03-11 00:46:59 +0900

    #1 [style] keyframes 테스트

--- src/pages/Login/page.tsx
+++ src/pages/Login/page.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
 import { theme } from '../../styles/theme';
+import { pop } from '../../styles/keyframes';
 
 const Tmp = styled.div({
   color: theme.colors.cornflowerblue,
+  animation: pop + ' 0.2s',
 });
 
 const LoginPage = () => {
