eff34aa06bf024d4a458b3a5962e840d22d76b52
Jadest <Jadest13x03@gmail.com>
2025-04-02 05:09:16 +0900

    #22 [feat] 사진 자세히보기 구현

--- src/components/Memo/Image/MemoImage.tsx
+++ src/components/Memo/Image/MemoImage.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
 import useModal from '@hooks/useModal';
 import CloseSVG from '@assets/icons/close.svg?react';
+import CategorySVG from '@assets/icons/category.svg?react';
+import ExpandSVG from '@assets/icons/expand.svg?react';
+import DownloadSVG from '@assets/icons/download.svg?react';
 
 const MemoImageContainer = styled.div({
   display: 'flex',
@@ -14,23 +17,60 @@ const MemoImageContainer = styled.div({
 const AboutImageContainer = styled.div({
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'end',
+  alignItems: 'start',
   background: 'white',
   borderRadius: '16px',
-  padding: '12px',
-  gap: '8px',
+  padding: '32px 0',
   ['img']: {
-    maxWidth: '640px',
+    margin: '24px 0',
+    maxWidth: '480px',
     maxHeight: '480px',
-    borderRadius: '12px',
   },
+});
+
+const AboutMoreCloseContainer = styled.div({
+  display: 'flex',
+  justifyContent: 'end',
+  width: '100%',
+  padding: '0 32px',
+  boxSizing: 'border-box',
+});
+
+const AboutMoreTitle = styled.p({
+  margin: '0',
+  fontSize: '24px',
+  fontWeight: '700',
+  lineHeight: '1.36',
+  textAlign: 'left',
+  padding: '0 32px',
+});
+
+const AboutImageContent = styled.div({
+  width: '100%',
+  padding: '0 32px',
+  display: 'flex',
+  gap: '15px',
+  boxSizing: 'border-box',
+});
+
+const GrowDiv = styled.div({
+  flexGrow: '1',
 });
 
 const AboutPhotoComponent = ({ closeModal, props }: { closeModal: () => void; props: { image: string } }) => {
   return (
     <AboutImageContainer>
-      <CloseSVG onClick={closeModal} />
+      <AboutMoreCloseContainer>
+        <CloseSVG onClick={closeModal} />
+      </AboutMoreCloseContainer>
+      <AboutMoreTitle>4월 여행계획</AboutMoreTitle>
       <img src={props.image} />
+      <AboutImageContent>
+        <CategorySVG />
+        <ExpandSVG />
+        <GrowDiv />
+        <DownloadSVG />
+      </AboutImageContent>
     </AboutImageContainer>
   );
 };
