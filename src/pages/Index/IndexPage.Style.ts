79252c529acd423135fd6cdb1ca773bcd5e77bf2
Jadest <Jadest13x03@gmail.com>
2025-05-27 20:26:13 +0900

    #77 [feat] 랜딩페이지 모듈화

---
+++ src/pages/Index/IndexPage.Style.ts
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+import { theme } from '@styles/theme';
+
+const IndexLayout = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+});
+
+const IndexContainer = styled.div({
+  flexGrow: '1',
+  overflow: 'hidden auto',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  background: theme.color.background.alternative,
+});
+
+const IndexContents = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+export { IndexLayout, IndexContainer, IndexContents };
--- src/pages/Index/page.tsx
+++ src/pages/Index/page.tsx
@@ -1,49 +1,25 @@
-import { StyledMain, StyledMainlayout } from '@layout/MainLayout/MainLayout.Style';
 import IndexFooter from '@components/Index/Footer/Footer';
 import IndexHeader from '@components/Index/Header/Header';
 import IndexFirstSection from '@components/Index/Section/First/FirstSection';
 import IndexLastSection from '@components/Index/Section/Last/LastSection';
 import IndexSecondSection from '@components/Index/Section/Second/SecondSection';
 import IndexThirdSection from '@components/Index/Section/Third/ThirdSection';
-import { theme } from '@styles/theme';
+import { IndexContainer, IndexContents, IndexLayout } from './IndexPage.Style';
 
 const IndexPage = () => {
   return (
-    <StyledMainlayout>
-      <StyledMain>
-        <IndexHeader />
-        <div
-          style={{
-            background: theme.color.background.alternative,
-            overflow: 'auto',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <div
-            style={{
-              width: '100%',
-            }}
-          >
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                overflow: 'hidden',
-              }}
-            >
-              <IndexFirstSection />
-              <IndexSecondSection />
-              <IndexThirdSection />
-              <IndexLastSection />
-            </div>
-          </div>
-          <IndexFooter />
-        </div>
-      </StyledMain>
-    </StyledMainlayout>
+    <IndexLayout>
+      <IndexHeader />
+      <IndexContainer>
+        <IndexContents>
+          <IndexFirstSection />
+          <IndexSecondSection />
+          <IndexThirdSection />
+          <IndexLastSection />
+        </IndexContents>
+        <IndexFooter />
+      </IndexContainer>
+    </IndexLayout>
   );
 };
 
