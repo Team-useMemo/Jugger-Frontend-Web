70480261e8d88936d86b95c2bbe32303c1fabffa
Jadest <Jadest13x03@gmail.com>
2025-06-04 20:51:48 +0900

    #77 [fix] 푸터 로고 색상 변경

--- src/router/Root.tsx
+++ src/router/Root.tsx
@@ -1,5 +1,5 @@
-import { Outlet, ScrollRestoration, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import { Outlet, ScrollRestoration, useNavigate } from 'react-router-dom';
 import Mainlayout from '@layout/MainLayout/MainLayout';
 import { webPath } from '.';
 
@@ -10,6 +10,7 @@ const Root = () => {
     const token = localStorage.getItem('accessToken');
     const currentPath = window.location.pathname;
     console.log(currentPath);
+    console.log(token);
     if (currentPath.startsWith('/oauth/callback/kakao') || currentPath.startsWith('/login')) {
       return; // 카카오 콜백 페이지는 리다이렉트 건너뜀
     }
