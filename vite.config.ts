0a6761b6abc557fd7d5b72ed493743509323cc20
Jadest13 <jadest13x03@gmail.com>
2025-07-21 18:02:50 +0900

    domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/Jugger-Frontend-Web/', // ← 여기에 GitHub 저장소 이름!
+  base: '/', // ← 여기에 GitHub 저장소 이름!
   build: {
     outDir: 'dist',
   },
