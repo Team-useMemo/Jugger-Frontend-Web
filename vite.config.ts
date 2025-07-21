3ca6aeb508b5f3536fbae21e0c57883e504c4232
Jadest13 <jadest13x03@gmail.com>
2025-07-21 17:03:28 +0900

    [fix] no domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/Jugger-Frontend-Web/', // ← 여기에 GitHub 저장소 이름!
   resolve: {
     alias: [
       { find: '@ts', replacement: '/src/ts' },
