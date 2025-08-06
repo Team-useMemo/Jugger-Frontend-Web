6a85a58f25dbdbf27efc534a7c72ef036b0ea3c2
Jadest <Jadest13x03@gmail.com>
2025-08-06 13:45:19 +0900

    #149 [feat] 메모 채팅 테마 디자인 반영

--- src/components/Memo/ImageDescription/MemoImageDescription.Style.ts
+++ src/components/Memo/ImageDescription/MemoImageDescription.Style.ts
@@ -1,31 +1,37 @@
 import styled from '@emotion/styled';
 import { media, theme } from '@styles/theme';
 
-const MemoImageDescriptionContainer = styled.div({
-  borderRadius: theme.radius[12],
-  maxWidth: '320px',
-  padding: '12px 16px',
-  background: theme.palette.blue[95],
-  boxSizing: 'border-box',
-  textAlign: 'left',
-  cursor: 'pointer',
+const MemoImageDescriptionContainer = styled.div(
+  ({ theme }) => ({
+    background: theme.mode === 'light' ? theme.palette.blue[95] : theme.color.background.alternativeinverse,
+    ['>p']: {
+      color: theme.color.label[theme.mode === 'light' ? 'neutral' : 'assistive'],
+    },
+  }),
+  {
+    borderRadius: theme.radius[12],
+    maxWidth: '320px',
+    padding: '12px 16px',
+    boxSizing: 'border-box',
+    textAlign: 'left',
+    cursor: 'pointer',
 
-  ['>p']: {
-    ...theme.font.caption1.medium,
-    color: theme.color.label.neutral,
-    margin: '0',
+    ['>p']: {
+      ...theme.font.caption1.medium,
+      margin: '0',
 
-    whiteSpace: 'pre-wrap',
-    wordBreak: 'break-word',
-    WebkitLineClamp: '2',
-    display: '-webkit-box',
-    WebkitBoxOrient: 'vertical',
-    overflow: 'hidden',
+      whiteSpace: 'pre-wrap',
+      wordBreak: 'break-word',
+      WebkitLineClamp: '2',
+      display: '-webkit-box',
+      WebkitBoxOrient: 'vertical',
+      overflow: 'hidden',
+    },
+
+    [media[480]]: {
+      maxWidth: '280px',
+    },
   },
-
-  [media[480]]: {
-    maxWidth: '280px',
-  },
-});
+);
 
 export { MemoImageDescriptionContainer };
